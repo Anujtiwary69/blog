61b2f83b996a5871fb10faeadfb233b878497281
anujtiwary69 <anujtiwary69@gmail.com>
2017-06-18 13:22:41 +0400

    chnages -
    s

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -16,8 +16,11 @@ class Controller extends BaseController
     public function searchPost(Request $request )
     {
     	$search = urlencode($request->input('search')); // keyword search 
-    	$combined_amzon = $this->AmazonData($search);
+    	// $combined_amzon = $this->AmazonData($search);
     	$combined_ebay = $this->EbayData($search);
+    	echo "<pre>";
+    	print_r($combined_ebay);
+    	// return view('welcome', ['amazon' => $combined_amzon,'ebay'=>$combined_ebay]);
     	
 
     }
@@ -26,10 +29,11 @@ class Controller extends BaseController
     {
     	$list = array(); // declare
     	$combined_amzon = array(); // declare
-    	include 'simple_html_dom.php'; 
+    	$combined_ebay = array();// delecalre
+    	include_once 'simple_html_dom.php'; 
     	$url = "https://www.amazon.co.uk/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=$search&rh=i%3Aaps%2Ck%3A$search";
     	$html = file_get_html($url);
-    	for($i=2;$i<20;$i++){
+    	for($i=2;$i<10;$i++){
     		foreach($html->find("li[id=result_$i]") as $element):
 	    			$img =$element->find('img',0);
 	    			$list['img'] = $img->src;
@@ -47,23 +51,25 @@ class Controller extends BaseController
     public function EbayData($search)
     {
     	$list = array(); // declare
-    	$combined_amzon = array(); // declare
-    	include 'simple_html_dom.php'; 
+    	$combined_ebay = array();// delecalre
+    	include_once 'simple_html_dom.php'; 
     	$url = "https://www.ebay.co.uk/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313.TR12.TRC2.A0.H0.Xipad.TRS0&_nkw=$search&_sacat=0";
     	$html = file_get_html($url);
-    	for($i=2;$i<20;$i++){
-    		foreach($html->find("li[id=result_$i]") as $element):
-	    			$img =$element->find('img',0);
-	    			$list['img'] = $img->src;
-	    			$list['title'] = $element->find('h2',0)->plaintext;
-	    			$list['price'] = $element->find('div[class=a-row a-spacing-none]',2)->plaintext;
-	    			$list['seller'] = $element->find('div[class=a-row a-spacing-none]',1)->plaintext;
-	    			// $list['StockLeft'] = $element->find('div[class=a-row a-spacing-none]',5)->plaintext;
-	    			$combined_amzon[] = $list;
-	       	endforeach;
-	    	
-	    }
-	    return $combined_amzon;
+    	$all_text= $html->find("li[class=sresult lvresult clearfix li shic]",0)->plaintext; 
+    	foreach($html->find("li[class=sresult lvresult clearfix li shic]") as $element):
+			$img =$element->find('img[class=img]',0);
+			$list['img'] = $img->src;
+			if(strpos('.gif', $list['img'])==false)
+			{
+				$list['title'] = $element->find('a[class=vip]',0)->plaintext;
+				$list['price'] = $element->find('span[class=bold]',0)->plaintext;
+				$list['seller'] = "none";
+				// $list['StockLeft'] = $element->find('div[class=a-row a-spacing-none]',5)->plaintext;
+				$combined_ebay[] = $list;
+			}
+			
+		endforeach;
+		return $combined_ebay;
     }
 
    
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,21 +3,81 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-sm-12">
+            <div class="col-sm-8">
                 <div class="">
                     <div class="clearfix"></div>
                     <br>
-                    <div class="input-group">
+                    <div class="">
                         <form action="<?php  echo url('/search');?>" method="post">
                             <input type="text" name="search" class="form-control" placeholder="Search for...">
                             {{ csrf_field() }}
+                            <br>
+                            <br>
                             <span class="input-group-btn">
-                                <button class="btn btn-default" type="submit" >Go!</button>
+                                <button class="btn btn-default" type="submit" style="float: right;" >Go!</button>
                             </span>
                         </form>
                         
                     </div>
                 </div>
             </div>
+        </div>
+
+        <!-- /table row stars -->
+        <div class="row">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <td>
+                            Image
+                        </td>
+                        <td>
+                        Product Name
+                        </td>
+                        <td>
+                            Product price
+                        </td>
+                        <td>
+                            Seller
+                        </td>
+                    </tr>
+                    <tbody>
+                       
+                        @foreach ($ebay as $user)
+                            <tr>
+                                <td>
+                                   <img src="{{ $user['img']}}"> 
+                                </td>
+                                <td style=" word-wrap: break-word;">
+                                {{$user['title']}}
+                                </td>
+                                <td>
+                                    {{$user['price']}}
+                                </td>
+                                <td>
+                                  {{$user['seller']}}
+                                </td>
+                            </tr>
+                        @endforeach
+                        @foreach ($ebay as $user)
+                            <tr>
+                                <td>
+                                   <img src="{{ $user['img']}}"> 
+                                </td>
+                                <td style=" word-wrap: break-word;">
+                                {{$user['title']}}
+                                </td>
+                                <td>
+                                    {{$user['price']}}
+                                </td>
+                                <td>
+                                  {{$user['seller']}}
+                                </td>
+                            </tr>
+                        @endforeach
+                       
+                    </tbody>
+                </thead>
+            </table>
         </div>
     </div>
